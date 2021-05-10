9256ffcd90f7700614b74f14b17d87d1574b3302
Andres Sainz <andresmsainz@gmail.com>
2021-05-09 22:51:19 -0400

    initial commit

---
+++ Abstraction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Polymorphism
+{
+    class Abstraction
+    {
+        static void Main()
+        {
+            // Cannot access abstract class directly
+            //Shapes_abs shapes = new Shapes_abs;
+
+            Lines_abs line = new Lines_abs();
+            line.SayHi();
+        }
+        
+        
+    }
+
+    abstract class Shapes_abs
+    {
+        public void SayHi()
+        {
+            Console.WriteLine("Hi From The Dark Abstract Side");
+        }
+    }
+
+    class Lines_abs : Shapes_abs
+    {
+
+    }
+}
